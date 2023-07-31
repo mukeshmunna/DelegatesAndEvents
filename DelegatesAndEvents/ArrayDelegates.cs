020eaa2235f66f4b3ff5f5f2bb0a25302ab8207f
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-07-31 12:14:11 +0530

    [Mukeh] add: Delegates And Events

---
+++ DelegatesAndEvents/ArrayDelegates.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    internal class ArrayDelegates
+    {
+        delegate void DelOp(int x, int y);
+        public static void SingleCastDelegate()
+        {
+            DelOp[] op =
+                {
+                new DelOp(Operation.Add),
+                new DelOp(Operation.sub)
+            };
+            for (int i = 0; i < op.Length; i++)
+            {
+                op[i](2, 3);
+                op[i](7, 8);
+                op[i](6, 5);
+
+            }
+        }
+        public static void MultiCastDelegate()
+        {
+            DelOp op = Operation.Add;
+            op += Operation.sub;
+            op(2, 1);
+            op(7, 3);    
+        }
+    }
+}
---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DelegatesAndEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayDelegates.SingleCastDelegate();
+            ArrayDelegates.MultiCastDelegate();
+
+            EventHandler.EventImplemet();
+
+        }
+    }
+}
